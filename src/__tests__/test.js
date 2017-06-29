ab74aa3969a9dff6499c3ce045049ba01b939c9e
Daniel Kostro <kostro.d@gmail.com>
2017-06-29 10:29:12 +0200

    Migrate tests to jest

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -9,36 +9,37 @@ var full = {
     matrix: [[3, 4], [1, 7]],
     labels: [0, 1]
 };
+
 describe('Confusion Matrix', function () {
     it('trivial', function () {
         const CM = new ConfusionMatrix(diagonal.matrix, diagonal.labels);
-        CM.matrix.should.deepEqual(diagonal.matrix);
-        CM.labels.should.deepEqual(diagonal.labels);
+        expect(CM.matrix).toBe(diagonal.matrix);
+        expect(CM.labels).toBe(diagonal.labels);
     });
 
 
     it('diagonal', function () {
         const CM = new ConfusionMatrix(diagonal.matrix, diagonal.labels);
-        CM.accuracy.should.equal(1);
-        CM.nbPredicted.should.equal(6);
+        expect(CM.accuracy).toBe(1);
+        expect(CM.nbPredicted).toBe(6);
     });
 
     it('full', function () {
         const CM = new ConfusionMatrix(full.matrix, full.labels);
-        CM.accuracy.should.equal(10 / 15);
-        CM.nbPredicted.should.equal(15);
+        expect(CM.accuracy).toBe(10 / 15);
+        expect(CM.nbPredicted).toBe(15);
     });
 
     it('should throw when matrix and labels do not have the same length', function () {
-        (function () {
+        expect(function () {
             new ConfusionMatrix([[1]], [1, 2]);
-        }).should.throw(/matrix and labels should have the same length/);
+        }).toThrowError(/matrix and labels should have the same length/);
     });
 
     it('should throw when the given matrix is not square', function () {
-        (function () {
+        expect(function () {
             new ConfusionMatrix([[1, 2]], []);
-        }).should.throw(/matrix must be square/);
+        }).toThrowError(/matrix must be square/);
     });
 
 
